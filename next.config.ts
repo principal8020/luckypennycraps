26790afdbf3180ff96347879d9a0fb74475c24ae
principal8020 <38128565+principal8020@users.noreply.github.com>
2026-09-01 15:07:02 +0000

    Add lay bets rebet undo and place presets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "localhost",
+    "127.0.0.1",
+    "*.app.github.dev",
+  ],
 };
 
 export default nextConfig;
